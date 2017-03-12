82e5dc0260dc4a996454d804d34cbe90c7ba7cf6
Gary Miller <gamiller286@gmail.com>
2017-03-12 13:54:13 -0500

    Started file for lab 6

---
+++ Lab6/Lab6/Debug/main.d
@@ -0,0 +1,139 @@
+main.d main.o: .././main.c \
+ ../../../../CEENBoT\ Files/lib-includes/capi324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/utils324v221.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ ../../../../CEENBoT\ Files/lib-includes/sys324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/psxc324v221.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.1.130\include/avr/iom324p.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h \
+ ../../../../CEENBoT\ Files/lib-includes/spi324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/tmrsrvc324v221.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ ../../../../CEENBoT\ Files/lib-includes/isr324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/lcd324v221.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ ../../../../CEENBoT\ Files/lib-includes/tiny324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/spkr324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/led324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/mega324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/step324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/swatch324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/usonic324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/uart324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/adc324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/spiflash324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/ti324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/i2c324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/cmucam324v221.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h \
+ ../../../../CEENBoT\ Files/lib-includes/pixy324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/bbtk324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/cbot324v221.h \
+ ../../../../CEENBoT\ Files/lib-includes/capi324v221.h
+
+../../../../CEENBoT\ Files/lib-includes/capi324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/utils324v221.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+../../../../CEENBoT\ Files/lib-includes/sys324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/psxc324v221.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.1.130\include/avr/iom324p.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
+
+../../../../CEENBoT\ Files/lib-includes/spi324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/tmrsrvc324v221.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+../../../../CEENBoT\ Files/lib-includes/isr324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/lcd324v221.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdarg.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+
+../../../../CEENBoT\ Files/lib-includes/tiny324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/spkr324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/led324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/mega324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/step324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/swatch324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/usonic324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/uart324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/adc324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/spiflash324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/ti324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/i2c324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/cmucam324v221.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\string.h:
+
+../../../../CEENBoT\ Files/lib-includes/pixy324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/bbtk324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/cbot324v221.h:
+
+../../../../CEENBoT\ Files/lib-includes/capi324v221.h:
